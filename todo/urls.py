86b32b6e07d0720dbc901910b549dd2117cd16e2
ivanb <bileichuk.ivan@gmail.com>
2024-07-24 12:19:59 +0300

    feat: Add CRUD views for Tag & Task models

---
+++ todo/urls.py
@@ -0,0 +1,69 @@
+from django.urls import path
+
+from todo.views import (
+    HomePageView,
+    toggle_task_status,
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
+
+urlpatterns = [
+    path(
+        "",
+        HomePageView.as_view(),
+        name="home"
+    ),
+    path(
+        "tasks/<int:pk>/toggle/",
+        toggle_task_status,
+        name="task-toggle"
+    ),
+    path(
+        "tasks/",
+        TaskListView.as_view(),
+        name="task-list"
+    ),
+    path(
+        "tasks/new/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "tasks/<int:pk>/edit/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list"
+    ),
+    path(
+        "tags/new/",
+        TagCreateView.as_view(),
+        name="tag-create"
+    ),
+    path(
+        "tags/<int:pk>/edit/",
+        TagUpdateView.as_view(),
+        name="tag-update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete"
+    ),
+]
+
+app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.shortcuts import (
+    render,
+    redirect,
+    get_object_or_404,
+)
 
-# Create your views here.
+from todo.models import Task, Tag
+from todo.forms import TagForm, TaskForm
+
+
+class HomePageView(generic.View):
+    template_name = "todo/home.html"
+
+    def get(self, request):
+        tasks = Task.objects.all().order_by("is_done", "-created_at")
+        return render(request, self.template_name, {"tasks": tasks})
+
+
+def toggle_task_status(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_done = not task.is_done
+    task.save()
+    return redirect("home")
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
