6906ac95f6734cc13b1962c0cd10e6b1944da16a
ivanb <bileichuk.ivan@gmail.com>
2024-07-24 14:09:38 +0300

    feat: Add DateTimeInpun widget to TaskForm

--- todo/forms.py
+++ todo/forms.py
@@ -13,3 +13,8 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["content", "deadline", "is_done", "tags"]
+        widgets = {
+            "deadline": forms.DateTimeInput(
+                attrs={"type": "datetime-local",
+                       "class": "form-control"}),
+        }
